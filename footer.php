7bade574bd0544e5971de526d0d3e17282cd990b
susanwrotethis <susanwrotethis@gmail.com>
2019-02-24 19:11:07 -0500

    add action hook

--- footer.php
+++ footer.php
@@ -13,6 +13,7 @@
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<?php get_sidebar( 'footer' ); ?>
+			<?php do_action( 'swt-jat-footer' ); ?>
 			<p class="site-copyright"><?php swt_jat_copyright(); ?></p>
 		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
